7f57e78d1fbead1383f886d7d046099a62fa2b70
pooja-c03 <pooja0909.c@gmail.com>
2025-02-27 10:27:51 +0530

    parse data

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({extended: true}));
+
 let tasks = [];
 app.get('/', (req, res)=>{
     res.render('index', {tasks: tasks});
